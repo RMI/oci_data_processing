b4159ab3df500670598402331b23280736c12423
rwang <rwang@rmi.org>
2021-12-21 15:58:43 -0500

    add aggregation code

--- data_for_webtool.py
+++ data_for_webtool.py
@@ -207,3 +207,211 @@ OCI_info20['Downstream Carbon Intensity (kgCO2eq/boe)'] = (OCI_info100['Downstre
 
 # Start Aggregation 
 
+## Aggregation to desired field/basin level
+agg_list = pd.read_csv('/Users/rwang/RMI/Climate Action Engine - Documents/OCI Phase 2/Upstream/aggregation_list_CAfields.csv')
+
+OCI_infobase_agg = pd.merge(OCI_infobase, agg_list,left_on = 'Field Name', right_on='Field name',how = 'left')
+
+OCI_infobase_aggregated = pd.concat([OCI_infobase_agg.groupby(['Country','Aggregation']).agg({'Assay Name':'first',
+                                             '2020 Total Oil and Gas Production Volume (boe)':'sum',
+                                             'Location':'first','Depth (ft)':'max',
+                                             'Gas shipped as LNG':'any',
+                                             'Enhanced recovery':'any','Fracked':'any',
+                                             'Default Refinery Configuration':'first',
+                                             'Heating Value Processed Oil and Gas (MJ/d)':'sum',
+                                             'Years in Production':'max',
+                                            'Number of Producing Wells':'sum',
+                                             '2020 Crude Production Volume (bbl)':'sum',
+                                            'Region':'first','Latitude':'mean','Longitude':'mean'}),
+OCI_infobase_agg.groupby(['Country','Aggregation']).apply(lambda gp:np.average(gp['API Gravity'], weights = gp['2020 Total Oil and Gas Production Volume (boe)'])).rename('API Gravity'),
+OCI_infobase_agg.groupby(['Country','Aggregation']).apply(lambda gp:np.average(gp['Sulfur Content (wt%)'], weights = gp['2020 Crude Production Volume (bbl)'])).rename('Sulfur Content Weight Percent'),
+OCI_infobase_agg.groupby(['Country','Aggregation']).apply(lambda gp:np.average(gp['Water-to-oil Ratio (bbl water/bbl oil)'], weights = gp['2020 Crude Production Volume (bbl)'])).rename('Water-to-oil Ratio (bbl water/bbl oil)'),
+OCI_infobase_agg.groupby(['Country','Aggregation']).apply(lambda gp:np.average(gp['Gas-to-Oil Ratio (scf/bbl)'], weights = gp['2020 Crude Production Volume (bbl)'])).rename('Gas-to-Oil Ratio (scf/bbl)'),
+OCI_infobase_agg.groupby(['Country','Aggregation']).apply(lambda gp:np.average(gp['Flaring-to-Oil Ratio (scf flared/bbl)'], weights = gp['2020 Crude Production Volume (bbl)'])).rename('Flaring-to-Oil Ratio (scf flared/bbl)'),
+OCI_infobase_agg.groupby(['Country','Aggregation']).apply(lambda gp:np.average(gp['Upstream Methane Intensity (kgCH4/boe)'], weights = gp['2020 Total Oil and Gas Production Volume (boe)'])).rename('Upstream Methane Intensity (kgCH4/boe)'),
+OCI_infobase_agg.groupby(['Country','Aggregation']).apply(lambda gp:np.average(gp['Midstream Methane Intensity (kgCH4/boe)'], weights = gp['2020 Total Oil and Gas Production Volume (boe)'])).rename('Midstream Methane Intensity (kgCH4/boe)'),
+OCI_infobase_agg.groupby(['Country','Aggregation']).apply(lambda gp:np.average(gp['Downstream Methane Intensity (kgCH4/boe)'], weights = gp['2020 Total Oil and Gas Production Volume (boe)'])).rename('Downstream Methane Intensity (kgCH4/boe)'),
+OCI_infobase_agg.groupby(['Country','Aggregation']).apply(lambda gp:np.average(gp['Total Methane Intensity (kgCH4/boe)'], weights = gp['2020 Total Oil and Gas Production Volume (boe)'])).rename('Total Methane Intensity (kgCH4/boe)'),
+OCI_infobase_agg.groupby(['Country','Aggregation']).apply(lambda gp:np.average(gp['Upstream Methane Emission Rate (NGSI Standard %)'], weights = gp['2020 Total Oil and Gas Production Volume (boe)'])).rename('Upstream Methane Emission Rate (NGSI Standard %)'),
+OCI_infobase_agg.groupby(['Country','Aggregation']).apply(lambda gp:np.average(gp['Upstream Methane Emission Rate (gCH4/Total MJ Produced)'], weights = gp['Total Energy Produced (MJ/d)'])).rename('Upstream Methane Emission Rate (gCH4/Total MJ Produced)'),
+OCI_infobase_agg.groupby(['Country','Aggregation']).apply(lambda gp:np.average(gp['Gas composition H2S'], weights = gp['2020 Total Oil and Gas Production Volume (boe)'])).rename('Gas composition H2S'),
+OCI_infobase_agg.groupby(['Country','Aggregation']).apply(lambda gp:np.average(gp['Gas composition CO2'], weights = gp['2020 Total Oil and Gas Production Volume (boe)'])).rename('Gas composition CO2'),
+OCI_infobase_agg.groupby(['Country','Aggregation']).apply(lambda gp:np.average(gp['Gas composition C1'], weights = gp['2020 Total Oil and Gas Production Volume (boe)'])).rename('Gas composition C1')],axis=1)
+
+# Post-aggregation field property value assignment
+## Flare rate is defined in the following categorical way
+def flare_rate_category(x):
+    flare_rate= x['Flaring-to-Oil Ratio (scf flared/bbl)']/(1+x['Gas-to-Oil Ratio (scf/bbl)']/5800)
+    if flare_rate<=1:
+        return 'Very Low Flare'
+    elif flare_rate>1 and flare_rate<=5:
+        return 'Low Flare'
+    elif flare_rate>5 and flare_rate<=50:
+        return 'Medium Flare'
+    elif flare_rate>50 and flare_rate<=500:
+        return 'High Flare'
+    elif flare_rate>500 and flare_rate<=1000:
+        return 'Very High Flare'
+    elif flare_rate>1000:
+        return 'Ultra High Flare'
+    else:
+        return 'No Flare Info'
+
+OCI_infobase_aggregated['Flare Rate']=OCI_infobase_aggregated.apply(lambda x: flare_rate_category(x),axis =1)
+
+OCI_infobase_aggregated.reset_index(inplace = True)
+
+OPEC_list = ['Equatorial Guinea','Gabon','Republic of the Congo','Iran','Iraq','Kuwait','Saudi Arabia',
+             'United Arab Emirates','Algeria','Libya','Venezuela','Angola','Nigeria','Russian Federation']
+
+OCI_infobase_aggregated['OPEC'] = OCI_infobase_aggregated['Country'].apply(lambda x:'Y' if x in OPEC_list else 'N')
+
+OCI_infobase_aggregated['Oil or Gas']=OCI_infobase_aggregated['Gas-to-Oil Ratio (scf/bbl)'].apply(lambda x: 'Gas' if x>5800 else 'Oil')
+
+def resource_type(x):
+    if x['API Gravity']<15 and x['Oil or Gas']=='Oil':
+        return 'Extra-Heavy Oil'
+    elif x['API Gravity']>=15 and x['API Gravity']<22 and x['Oil or Gas']=='Oil':
+        return 'Heavy Oil' 
+    elif x['API Gravity']>=22 and x['API Gravity']<32 and x['Oil or Gas']=='Oil':
+        return 'Light Oil'
+    elif x['API Gravity']>=32 and x['API Gravity']<42 and x['Oil or Gas']=='Oil':
+        return 'Medium Oil'
+    elif x['API Gravity']>=42 and x['API Gravity']<51 and x['Oil or Gas']=='Oil':
+        return 'Ultra-Light Oil'
+    elif x['API Gravity']>=51:
+        return 'Condensate'
+    elif x['Oil or Gas'] =='Gas' and x['Aggregation'] in ['Sulige', 'Powder River', 'Fairview', 'Roma', 'Condabri', 'Talinga', 'Orana', 'Uinta']:
+        return 'Coal-bed Gas'
+    elif x['Gas composition C1']>85 and x['Oil or Gas'] =='Gas' :
+        return 'Dry Gas'
+    elif x['Gas composition C1']<=85 and x['Oil or Gas'] =='Gas':
+        return 'Wet Gas'
+    else: 
+        return 'Uncategorized'
+    
+
+OCI_infobase_aggregated['Resource Type']=OCI_infobase_aggregated.apply(lambda x: resource_type(x),axis =1)
+
+def sweet_sour(x):
+    if x['Oil or Gas']=='Oil' and x['Sulfur Content Weight Percent']>0.5:
+        return 'Sour Oil'
+    elif x['Oil or Gas']=='Oil' and x['Sulfur Content Weight Percent']<0.42:
+        return 'Sweet Oil'
+    elif x['Oil or Gas']=='Gas' and x['Gas composition CO2']>10:
+        return 'Acid Gas'
+    elif x['Oil or Gas']=='Gas' and x['Gas composition H2S']>2:
+        return 'Sour Gas'
+    elif x['Oil or Gas']=='Gas' and x['Gas composition H2S']<=2:
+        return 'Sweet Gas'
+    else:
+        return 'Uncategorized'
+OCI_infobase_aggregated['Sour or Sweet'] = OCI_infobase_aggregated.apply(lambda x: sweet_sour(x),axis =1)
+
+OCI_infobase_aggregated.rename(columns = {'Aggregation':'Field Name','Depth (ft)':'Max Depth(ft)'}, inplace = True)
+
+OCI_infobase_aggregated = OCI_infobase_aggregated.round(2)
+
+OCI_infobase_aggregated['descriptor']= (
+        OCI_infobase_aggregated['Field Name']
+        + ' is an '
+        + OCI_infobase_aggregated['Location']
+        + ' '
+        + OCI_infobase_aggregated['Oil or Gas']
+        + ' field located in '
+        + OCI_infobase_aggregated['Country']
+        + '. '
+        + 'It has an API graivty of '
+        + OCI_infobase_aggregated['API Gravity'].round(0).astype(str)
+        + ' and a sulfur content of '
+        + OCI_infobase_aggregated['Sulfur Content Weight Percent'].round(2).astype(str)
+        + ', characterizing it as a '
+        + OCI_infobase_aggregated['Resource Type']
+        + ' and a '
+        + OCI_infobase_aggregated['Sour or Sweet']
+        + '.')
+        
+
+OCI_infobase_aggregated = OCI_infobase_aggregated[(OCI_infobase_aggregated['Field Name']!='Amenamkpono') & (OCI_infobase_aggregated['Field Name']!='Rincon del Mangrullo') & (OCI_infobase_aggregated['Field Name']!='Brent')]
+
+OCI_infobase_aggregated.to_csv('/Users/rwang/RMI/Climate Action Engine - Documents/OCI Phase 2/Webtool updates/basedata/infobase.csv',index = False)
+
+OCI_info100['2020 Total Oil and Gas Production Volume (boe)']= OCI_infobase['2020 Total Oil and Gas Production Volume (boe)']
+
+OCI_info100_agg = pd.merge(OCI_info100, agg_list,left_on = 'Field Name', right_on='Field name',how = 'left')
+
+
+def w_avg(x,column_to_be_averaged,weight):
+    return(np.average(x[column_to_be_averaged], 
+                      weights = x[weight]))
+
+columns_to_be_averaged = ['Upstream: Exploration (kgCO2eq/boe)',
+ 'Upstream: Drilling & Development (kgCO2eq/boe)',
+ 'Upstream: Crude Production & Extraction (kgCO2eq/boe)',
+ 'Upstream: Surface Processing (kgCO2eq/boe)',
+ 'Upstream: Maintenance (kgCO2eq/boe)',
+ 'Upstream: Waste Disposal (kgCO2eq/boe)',
+ 'Upstream: Crude Transport (kgCO2eq/boe)',
+ 'Upstream: Other Small Sources (kgCO2eq/boe)',
+ 'Upstream: Offsite emissions credit/debit (kgCO2eq/boe)',
+ 'Upstream: Carbon Dioxide Sequestration (kgCO2eq/boe)',
+ 'Upstream Carbon Intensity (kgCO2eq/boe)',
+ 'Midstream: Electricity (kgCO2eq/boe)',
+ 'Midstream: Heat (kgCO2eq/boe)',
+ 'Midstream: Steam (kgCO2eq/boe)',
+ 'Midstream: Hydrogen via SMR (kgCO2eq/boe)',
+ 'Midstream: Hydrogen via CNR (kgCO2eq/boe)',
+ 'Midstream: Other Emissions (kgCO2eq/boe)',
+ 'Midstream Carbon Intensity (kgCO2eq/boe)',
+ 'Downstream: Transport to Consumers (kgCO2eq/boe)',
+ 'Downstream: Gasoline for Cars (kgCO2eq/boe)',
+ 'Downstream: Jet Fuel for Planes (kgCO2eq/boe)',
+ 'Downstream: Diesel for Trucks and Engines (kgCO2eq/boe)',
+ 'Downstream: Fuel Oil for Boilers (kgCO2eq/boe)',
+ 'Downstream: Petroleum Coke for Power (kgCO2eq/boe)',
+ 'Downstream: Liquid Heavy Ends for Ships (kgCO2eq/boe)',
+ 'Downstream: Natural Gas Liquids (kgCO2eq/boe)',
+ 'Downstream: Liquefied Petroleum Gases (kgCO2eq/boe)',
+ 'Downstream: Petrochemical Feedstocks (kgCO2eq/boe)',
+ 'Dwonstream: Natural Gas (kgCO2eq/boe)',
+ 'Downstream Carbon Intensity (kgCO2eq/boe)',
+ 'Total Emission Carbon Intensity (kgCO2eq/boe)',
+ 'Industry GHG Responsibility (kgCO2eq/boe)',
+ 'Consumer GHG Responsibility (kgCO2eq/boe)']
+
+OCI_info100_aggregated = pd.concat([
+OCI_info100_agg.groupby(['Country','Aggregation']).apply(
+    lambda x:w_avg(x,col,'2020 Total Oil and Gas Production Volume (boe)')).rename(col) 
+    for col in columns_to_be_averaged],axis=1)
+
+OCI_info100_aggregated.reset_index(inplace = True)
+
+OCI_info100_aggregated.rename(columns = {'Aggregation':'Field Name'},inplace = True)
+
+OCI_info100_aggregated = OCI_info100_aggregated.round(2)
+
+OCI_info100_aggregated = OCI_info100_aggregated[(OCI_info100_aggregated['Field Name']!='Amenamkpono') & (OCI_info100_aggregated['Field Name']!='Rincon del Mangrullo') & (OCI_info100_aggregated['Field Name']!='Brent')]
+
+OCI_info100_aggregated.to_csv('/Users/rwang/RMI/Climate Action Engine - Documents/OCI Phase 2/Webtool updates/basedata/info100.csv',index = False)
+
+OCI_info20['2020 Total Oil and Gas Production Volume (boe)']= OCI_infobase['2020 Total Oil and Gas Production Volume (boe)']
+
+OCI_info20_agg = pd.merge(OCI_info20, agg_list,left_on = 'Field Name', right_on='Field name',how = 'left')
+
+
+columns_to_be_averaged = ['Upstream Carbon Intensity (kgCO2eq/boe)',
+       'Midstream Carbon Intensity (kgCO2eq/boe)',
+       'Downstream Carbon Intensity (kgCO2eq/boe)']
+
+OCI_info20_aggregated = pd.concat([
+OCI_info20_agg.groupby(['Country','Aggregation']).apply(
+    lambda x:w_avg(x,col,'2020 Total Oil and Gas Production Volume (boe)')).rename(col) 
+    for col in columns_to_be_averaged],axis=1)
+
+OCI_info20_aggregated.reset_index(inplace = True)
+OCI_info20_aggregated.rename(columns = {'Aggregation':'Field Name'},inplace = True)
+
+OCI_info20_aggregated = OCI_info20_aggregated.round(2)
+
+OCI_info20_aggregated = OCI_info20_aggregated[(OCI_info20_aggregated['Field Name']!='Amenamkpono') & (OCI_info20_aggregated['Field Name']!='Rincon del Mangrullo') & (OCI_info20_aggregated['Field Name']!='Brent')]
+OCI_info20_aggregated.to_csv('/Users/rwang/RMI/Climate Action Engine - Documents/OCI Phase 2/Webtool updates/basedata/info20.csv',index = False)
