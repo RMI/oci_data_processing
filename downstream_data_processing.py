4a75768e01c6c95e4ff052041d4b623e01f4a324
rwang <rwang@rmi.org>
2022-05-20 08:59:49 -0400

    run opem in both gwp100 and gwp 20

--- downstream_data_processing.py
+++ downstream_data_processing.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import numpy as np
 import os
+pd.options.mode.chained_assignment = None 
 sp_dir = '/Users/rwang/RMI/Climate Action Engine - Documents/OCI Phase 2'
 opem_dir = '/Users/rwang/Documents/OCI+/Downstream/opem'
 
 print('Merging upstream and midstream results...')
 
-import sqlite3
-connection = sqlite3.connect("../OCI_Database.db")
+# import sqlite3
+# connection = sqlite3.connect("../OCI_Database.db")
 
-upstream = pd.read_sql('select * from upstream_results',connection)
-midstream = pd.read_sql('select * from midstream_results',connection)
+upstream = pd.read_csv(sp_dir + '/Upstream/upstream_data_pipeline_sp/Postprocessed_Outputs_2/upstream_postprocessed.csv')
+midstream = pd.read_csv(sp_dir + '/Upstream/upstream_data_pipeline_sp/Postprocessed_Outputs_2/midstream_postprocessed.csv')
+
 
 # Calculate Crude to Refinery in bbl/d from Energy Summary tab of OPGEE model. Formula is based on cell G6 in OPEM input tab  
 #https://rockmtnins.sharepoint.com/:x:/r/sites/CAE/_layouts/15/Doc.aspx?sourcedoc=%7B5E0994C9-8E35-440B-8BB4-31DF5167F60C%7D&file=OCI%20site%20input%20table%20sources.xlsx&action=default&mobileredirect=true&cid=544ad233-565f-438c-9ce2-d7d5b607b1da
@@ -50,129 +52,131 @@ upstream['NGL_C5(boed)'] = (upstream['ES_NGL_output(mmbtu/d)']-(((upstream['FS_L
 # 1000*(Flowsheet!HG7-'Energy Summary'!E77/'Energy Summary'!M135)
 upstream['petcoke(kg/d)'] = 1000*(upstream['FS_Petcoke_to_stock(t/d)']-upstream['ES_Petcoke_fuel(mmbtu/d)']/upstream['ES_Energy_Density_petcoke(mmbtu/t)'])
 
+
 upstream_midstream = upstream.merge(midstream,right_on=
-        ['opgee_field','opgee_country','original_file'],left_on = 
-        ['Field_name', 'Field location (Country)','original_file'], how = 'left')
+        ['Field name','Field location (Country)','gwp'],left_on = 
+        ['Field_name', 'Field location (Country)','gwp'], how = 'left',indicator = True)
+if upstream_midstream[upstream_midstream['_merge']!='both'].shape[0]!=0:
+    print('not fully merged. please check')
+    print(upstream_midstream[upstream_midstream['_merge']!='both'])
+else:
+    upstream_midstream.drop(columns = '_merge',inplace = True)    
+def run_opem(upstream_midstream, gwp):   
+    upstream_midstream_for_opem = upstream_midstream[upstream_midstream['gwp']==gwp]
+    upstream_midstream_for_opem['OPEM_field_name']=upstream_midstream_for_opem['Field_name']+';'+upstream_midstream_for_opem['original_file']
 
-#upstream_midstream[upstream_midstream['sulfur'].isna()][['Field name','year']]
+    print('Getting data for OPEM Product Slates...')
 
-# Filter out obsolete and misinput upstream fields such as Bakken , Generic 
-upstream_midstream = upstream_midstream[upstream_midstream['sulfur'].notna()]
+    upstream_midstream_for_opem['volume_flow_bbl']=''
+    upstream_midstream_for_opem['Product Slate (bbl product per day)'] = ''
+    upstream_midstream_for_opem['energy_flow_MJ']=''
+    upstream_midstream_for_opem['mass_flow_kg']=''
+    upstream_midstream_for_opem['Liquefied Petroleum Gases (LPG)_bbl']= upstream_midstream_for_opem['Liquified Petroleum Gas (LPG).1']/(2.04*0.159*270)
+    upstream_midstream_for_opem['Petrochemical Feedstocks_bbl']=upstream_midstream_for_opem['Petrochemical Feedstocks.1']/(1.264*0.159*270)
+    upstream_midstream_for_opem['Asphalt_bbl']= 0
 
-upstream_midstream_for_opem = upstream_midstream #[upstream_midstream['year']==2020]
+    upstream_midstream_for_opem['emission(kgCO2eq/bbl)']='kgCO2eq/bbl'
+    upstream_midstream_for_opem['Per energy content of refinery product (gCO2eq/MJ)'] = 'Emission by Product (gCO2eq/MJ)'
+    upstream_midstream_for_opem['Allocation to gasoline (g CO2eq/MJ)']='Process Emission (gCO2eq/MJ)'
 
-upstream_midstream_for_opem['OPEM_field_name']=upstream_midstream_for_opem['opgee_field']+';'+upstream_midstream_for_opem['original_file']
+    opem_product_slate =  upstream_midstream_for_opem[['OPEM_field_name',                                                   
+                                                    'Product Slate (bbl product per day)',
+                                                    'volume_flow_bbl','throughput','Gasoline',
+    'Jet Fuel',
+    'Diesel',
+    'Fuel Oil',
+    'Petroleum Coke',
+    'Residual Fuels',
+    'Refinery Fuel Gas (RFG)',
+    'Liquefied Petroleum Gases (LPG)_bbl',
+    'Petrochemical Feedstocks_bbl',
+    'Asphalt_bbl',                                                   
+    'energy_flow_MJ',
+    'Blended Gasoline',
+    'Jet-A/AVTUR',
+    'ULSD',
+    'Fuel Oil.1',
+    'Coke',
+    'Liquid Heavy Ends',
+    'RFG',
+    'Surplus NCR H2',
+    'Liquified Petroleum Gas (LPG)',
+    'Petrochemical Feedstocks',
+    'Asphalt',
+    'Gasoline S wt%',
+    'Gasoline H2 wt%','mass_flow_kg',
+    'Blended Gasoline.1',
+    'Jet-A/AVTUR.1',
+    'ULSD.1',
+    'Fuel Oil.2',
+    'Coke.1',
+    'Liquid Heavy Ends.1',
+    'Sulphur',
+    'RFG.1',
+    'Surplus NCR H2.1',
+    'Liquified Petroleum Gas (LPG).1',
+    'Petrochemical Feedstocks.1',
+    'Asphalt.1']]
 
-print('Getting data for OPEM Product Slates...')
+    opem_product_slate = opem_product_slate.T
 
-upstream_midstream_for_opem['volume_flow_bbl']=''
-upstream_midstream_for_opem['Product Slate (bbl product per day)'] = ''
-upstream_midstream_for_opem['energy_flow_MJ']=''
-upstream_midstream_for_opem['mass_flow_kg']=''
-upstream_midstream_for_opem['Liquefied Petroleum Gases (LPG)_bbl']= upstream_midstream_for_opem['Liquified Petroleum Gas (LPG).1']/(2.04*0.159*270)
-upstream_midstream_for_opem['Petrochemical Feedstocks_bbl']=upstream_midstream_for_opem['Petrochemical Feedstocks.1']/(1.264*0.159*270)
-upstream_midstream_for_opem['Asphalt_bbl']= 0
+    opem_product_slate.columns = opem_product_slate.iloc[0]
+    opem_product_slate = opem_product_slate.iloc[1:,:]
 
-upstream_midstream_for_opem['emission(kgCO2eq/bbl)']='kgCO2eq/bbl'
-upstream_midstream_for_opem['Per energy content of refinery product (gCO2eq/MJ)'] = 'Emission by Product (gCO2eq/MJ)'
-upstream_midstream_for_opem['Allocation to gasoline (g CO2eq/MJ)']='Process Emission (gCO2eq/MJ)'
+    slate_index = pd.read_csv(opem_dir + '/src/opem/products/product_slates/all_product_slates.csv')
 
-opem_product_slate =  upstream_midstream_for_opem[['OPEM_field_name',                                                   
-                                                   'Product Slate (bbl product per day)',
-                                                   'volume_flow_bbl','throughput','Gasoline',
- 'Jet Fuel',
- 'Diesel',
- 'Fuel Oil',
- 'Petroleum Coke',
- 'Residual Fuels',
- 'Refinery Fuel Gas (RFG)',
-'Liquefied Petroleum Gases (LPG)_bbl',
-'Petrochemical Feedstocks_bbl',
-'Asphalt_bbl',                                                   
-'energy_flow_MJ',
-'Blended Gasoline',
- 'Jet-A/AVTUR',
- 'ULSD',
- 'Fuel Oil.1',
- 'Coke',
- 'Liquid Heavy Ends',
- 'RFG',
- 'Surplus NCR H2',
- 'Liquified Petroleum Gas (LPG)',
- 'Petrochemical Feedstocks',
- 'Asphalt',
- 'Gasoline S wt%',
- 'Gasoline H2 wt%','mass_flow_kg',
- 'Blended Gasoline.1',
- 'Jet-A/AVTUR.1',
- 'ULSD.1',
- 'Fuel Oil.2',
- 'Coke.1',
- 'Liquid Heavy Ends.1',
- 'Sulphur',
- 'RFG.1',
- 'Surplus NCR H2.1',
- 'Liquified Petroleum Gas (LPG).1',
- 'Petrochemical Feedstocks.1',
- 'Asphalt.1']]
+    opem_product_slate.index = slate_index.iloc[:,0]
 
-opem_product_slate = opem_product_slate.T
+    #opem_product_slate.to_excel('../Downstream/Analytics/all_product_slates.xlsx')
+    opem_product_slate.to_csv(opem_dir+'/src/opem/products/product_slates/all_product_slates.csv')
 
-opem_product_slate.columns = opem_product_slate.iloc[0]
-opem_product_slate = opem_product_slate.iloc[1:,:]
+    print('Preparing data for opem_input...')
 
-slate_index = pd.read_csv(opem_dir + '/src/opem/products/product_slates/all_product_slates.csv')
+    opem_input = pd.DataFrame()
+    opem_input['OPEM_field_name']=upstream_midstream_for_opem['OPEM_field_name']
+    opem_input['Upstream Field Selection']=''
+    opem_input['Gas Production Volume (MCFD)']=upstream_midstream_for_opem['ES_Gas_output(mmbtu/d)']*1000/983  
+    opem_input['Oil Production Volume (BOED)']=upstream_midstream_for_opem['crude_to_refinery(bbl/d)']
+    opem_input['NGL Volume source']=2
+    opem_input['NGL C2 Volume (BOED)']=upstream_midstream_for_opem['NGL_C2(boed)']
+    opem_input['NGL C3 Volume (BOED)']=upstream_midstream_for_opem['NGL_C3(boed)']
+    opem_input['NGL C4 Volume (BOED)']=upstream_midstream_for_opem['NGL_C4(boed)']
+    opem_input['NGL C5+ Volume (BOED)']=upstream_midstream_for_opem['NGL_C5(boed)']
+    opem_input['Total field NGL volume (BOED)']=''
+    opem_input['OPGEE Coke mass (kg/d)']=upstream_midstream_for_opem['petcoke(kg/d)']
+    opem_input['% Field NGL C2 Volume allocated to Ethylene converstion']=1
+    opem_input['GWP selection (yr period, 100 or 20)']=gwp
 
-opem_product_slate.index = slate_index.iloc[:,0]
+    opem_input_T = opem_input.set_index('OPEM_field_name').T
 
-opem_product_slate.to_excel('../Downstream/Analytics/all_product_slates.xlsx')
-opem_product_slate.to_csv(opem_dir+'/src/opem/products/product_slates/all_product_slates.csv')
+    #opem_input_T.to_excel('../Downstream/Analytics/opem_input.xlsx')
 
-print('Preparing data for opem_input...')
+    opem_input_index = pd.read_csv(opem_dir + '/opem_input.csv',header=0)
+    opem_input_T.reset_index(inplace = True)
 
-opem_input = pd.DataFrame()
-opem_input['OPEM_field_name']=upstream_midstream_for_opem['OPEM_field_name']
-opem_input['Upstream Field Selection']=''
-opem_input['Gas Production Volume (MCFD)']=upstream_midstream_for_opem['ES_Gas_output(mmbtu/d)']*1000/983  
-opem_input['Oil Production Volume (BOED)']=upstream_midstream_for_opem['crude_to_refinery(bbl/d)']
-opem_input['NGL Volume source']=2
-opem_input['NGL C2 Volume (BOED)']=upstream_midstream_for_opem['NGL_C2(boed)']
-opem_input['NGL C3 Volume (BOED)']=upstream_midstream_for_opem['NGL_C3(boed)']
-opem_input['NGL C4 Volume (BOED)']=upstream_midstream_for_opem['NGL_C4(boed)']
-opem_input['NGL C5+ Volume (BOED)']=upstream_midstream_for_opem['NGL_C5(boed)']
-opem_input['Total field NGL volume (BOED)']=''
-opem_input['OPGEE Coke mass (kg/d)']=upstream_midstream_for_opem['petcoke(kg/d)']
-opem_input['% Field NGL C2 Volume allocated to Ethylene converstion']=1
-opem_input['GWP selection (yr period, 100 or 20)']=100
+    # Get the index from opem_input.csv and update it with opem input values
+    df = pd.concat([opem_input_index.iloc[:,:5],opem_input_T.iloc[:,1:]],axis = 1)
 
-opem_input_T = opem_input.set_index('OPEM_field_name').T
+    df.to_csv('./opem_input.csv',index=False)
 
-opem_input_T.to_excel('../Downstream/Analytics/opem_input.xlsx')
+    print('Running opem...')
+    os.system('opem')
 
-opem_input_index = pd.read_csv(opem_dir + '/opem_input.csv',header=0)
-opem_input_T.reset_index(inplace = True)
+    opem_output = pd.read_csv('./opem_output.csv',header=1)
 
-# Get the index from opem_input.csv and update it with opem input values
-df = pd.concat([opem_input_index.iloc[:,:5],opem_input_T.iloc[:,1:]],axis = 1)
+    #upstream_midstream_for_opem['estimate_boe/d'] = upstream_midstream_for_opem['Oil production volume']*(1+upstream_midstream_for_opem['Gas-to-oil ratio (GOR)']/5800)
 
-df.to_csv('./opem_input.csv',index=False)
+    opem_output_T = opem_output.set_index('Oil_Selected').T
 
-print('Running opem...')
-os.system('opem')
+    opem_output_T.reset_index(inplace = True)
 
-opem_output = pd.read_csv('./opem_output.csv',header=1)
-
-#upstream_midstream_for_opem['estimate_boe/d'] = upstream_midstream_for_opem['Oil production volume']*(1+upstream_midstream_for_opem['Gas-to-oil ratio (GOR)']/5800)
-
-opem_output_T = opem_output.set_index('Selected Oil').T
-
-opem_output_T.reset_index(inplace = True)
-
-# Save and reload to get unique column headers
-opem_output_T.to_excel('opem_output_1.xlsx',index=False)
-opem_output_T = pd.read_excel('opem_output_1.xlsx')
-up_mid_down = upstream_midstream_for_opem.merge(opem_output_T,left_on='OPEM_field_name',right_on ='index',how='left')
-up_mid_down.to_excel('../Downstream/Analytics/up_mid_down_new_100yr.xlsx')
-up_mid_down.to_sql('up_mid_downstream_results',connection, if_exists='replace', index=False)
-print('OPEM run completed and up_mid_down file updated.')
-
+    # Save and reload to get unique column headers
+    opem_output_T.to_excel('opem_output_1.xlsx',index=False)
+    opem_output_T = pd.read_excel('opem_output_1.xlsx')
+    up_mid_down = upstream_midstream_for_opem.merge(opem_output_T,left_on='OPEM_field_name',right_on ='index',how='left')
+    print('OPEM run completed and up_mid_down file updated for gwp ' +str(gwp)) 
+    return up_mid_down
+up_mid_down_100 = run_opem(upstream_midstream, 100)
+up_mid_down_20 = run_opem(upstream_midstream, 20)
+up_mid_down = pd.concat([up_mid_down_100,up_mid_down_20])
+up_mid_down.to_csv(sp_dir+'/Upstream/upstream_data_pipeline_sp/Postprocessed_outputs_2/downstream_postprocessed.csv')
